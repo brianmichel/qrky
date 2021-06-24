f8b9294d30d6da08241388128d45f9f676b64663
Brian Michel <brian.michel@gmail.com>
2021-06-23 21:01:06 -0400

    Initial Commit

---
+++ qrky/qrkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrkyApp.swift
+//  qrky
+//
+//  Created by Brian Michel on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct qrkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
