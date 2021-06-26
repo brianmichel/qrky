2456777a39404650e01f3c8aa6ea181f4f3ad3ca
Brian Michel <brian.michel@gmail.com>
2021-06-26 13:35:23 -0400

    Add app delegate to terminate the app

--- qrky/qrkyApp.swift
+++ qrky/qrkyApp.swift
@@ -7,8 +7,18 @@
 
 import SwiftUI
 
+/// Create an AppDelegate to terminate the application when the last window is closed.
+/// This is a hack around SwiftUI for the time being...
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct qrkyApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             HomeView()
